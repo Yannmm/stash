92f8162efc42a965f5fa71c393d5b234b05b023a
yannmm <yannmm@foxmail.com>
2025-01-28 22:06:41 +0800

    Initial Commit

---
+++ StashTests/StashTests.swift
@@ -0,0 +1,17 @@
+//
+//  StashTests.swift
+//  StashTests
+//
+//  Created by Yan Meng on 2025/1/28.
+//
+
+import Testing
+@testable import Stash
+
+struct StashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
