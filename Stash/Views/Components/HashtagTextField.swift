3c2ae232a882bc3d8532d6f93ba37362e560f522
yannmm <yannmm@foxmail.com>
2025-06-22 23:42:04 +0800

    Temp save.

--- Stash/Views/Components/HashtagTextField.swift
+++ Stash/Views/Components/HashtagTextField.swift
@@ -9,27 +9,8 @@ import SwiftUI
 
 struct HashtagTextField: NSViewRepresentable {
     @Binding var text: String
-    
     var font: NSFont?
-    
     var onCommit: () -> Void = {}
-    
-    internal class Coordinator: NSObject, NSTextFieldDelegate {
-        var parent: HashtagTextField
-        
-        init(_ parent: HashtagTextField) {
-            self.parent = parent
-        }
-        
-        func controlTextDidChange(_ obj: Notification) {
-            guard let textField = obj.object as? NSTextField else { return }
-            parent.text = textField.stringValue
-        }
-        
-        func controlTextDidEndEditing(_ obj: Notification) {
-            parent.onCommit()
-        }
-    }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -65,6 +46,57 @@ struct HashtagTextField: NSViewRepresentable {
                 editor.scrollRangeToVisible(range)
             }
         }
+    }
+    
+    internal class Coordinator: NSObject, NSTextFieldDelegate {
+        var parent: HashtagTextField
+        
+        init(_ parent: HashtagTextField) {
+            self.parent = parent
+        }
+        
+        func controlTextDidChange(_ obj: Notification) {
+            guard let textField = obj.object as? NSTextField else { return }
+            
+            let text = textField.stringValue
+            if let range = text.range(of: #"(?<=\s)#\w*$"#, options: .regularExpression) {
+                print("Matched:", text[range])
+                // Show popover
+                showSuggestions(textField)
+            } else {
+                popover.performClose(nil)
+            }
+            
+            parent.text = textField.stringValue
+        }
+        
+        func controlTextDidEndEditing(_ obj: Notification) {
+            parent.onCommit()
+        }
+        
+        func showSuggestions(_ textField: NSTextField) {
+            guard
+                let fieldEditor = textField.window?.fieldEditor(false, for: textField) as? NSTextView else { return }
+            
+            let selectedRange = fieldEditor.selectedRange()
+            let rect = fieldEditor.firstRect(forCharacterRange: selectedRange, actualRange: nil)
+            let localRect = fieldEditor.convert(rect, to: nil) // convert to window coords
+            popover.show(relativeTo: rect, of: textField, preferredEdge: .minY)
+        }
+        
+        private lazy var popover: NSPopover = {
+            // Setup the popover
+            let popover = NSPopover()
+                    let hintViewController = NSViewController()
+                    hintViewController.view = NSView(frame: NSRect(x: 0, y: 0, width: 150, height: 50))
+                    hintViewController.view.wantsLayer = true
+                    hintViewController.view.layer?.backgroundColor = NSColor.systemYellow.cgColor
+
+                    popover.contentViewController = hintViewController
+                    popover.behavior = .semitransient // or .transient
+            
+            return popover
+        }()
     }
 }
 
