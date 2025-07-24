295043aa2a551c9351298a460377ad73a8167ce0
yannmm <yannmm@foxmail.com>
2025-07-24 17:34:02 +0800

    To fix errors.

--- Stash/Views/Components/Menu/MenuItemView.swift
+++ Stash/Views/Components/Menu/MenuItemView.swift
@@ -30,30 +30,22 @@ struct _MenuItemView: View {
             VStack(alignment: .leading, spacing: 1) {
                 Text(item.title)
                     .font(.system(size: 13))
-                    .foregroundColor(item.enabled ?
-                                     (isCurrentlyHovered ? .white : .primary) :
-                            .secondary)
+                    .foregroundColor(isCurrentlyHovered ? .white : .primary)
                 
-                if let detail = item.detail {
-                    Text(detail)
-                        .font(.system(size: 11))
-                        .foregroundColor(item.enabled ?
-                                         (isCurrentlyHovered ? Color.white.opacity(0.8) : .secondary) :
-                                .accentColor)
-                }
+                Text(item.detail)
+                    .font(.system(size: 11))
+                    .foregroundColor((isCurrentlyHovered ? Color.white.opacity(0.8) : .secondary))
             }
             
             Spacer()
         }
         .background(
             Rectangle()
-                .fill(isCurrentlyHovered && item.enabled ?
-                      Color(NSColor.controlAccentColor) :
-                        Color.clear)
+                .fill(Color(NSColor.controlAccentColor))
         )
         .contentShape(Rectangle())
         .onHover { hovering in
-            if hovering && item.enabled {
+            if hovering  {
                 withAnimation(.easeInOut(duration: 0.1)) {
                     hoveredItem = item.id
                 }
