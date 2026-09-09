d7360261efaf90dc2f0f3fe6cdddd65f4e05df19
yannmm <yannmm@foxmail.com>
2026-09-09 21:59:51 +0800

    fix: make prettyTimestamp nil for back-compatible

--- Stash/Synchronization/Sidecar.swift
+++ Stash/Synchronization/Sidecar.swift
@@ -3,7 +3,7 @@ import Foundation
 struct Sidecar: Codable, Equatable {
     let uid: UUID
     let timestamp: Date
-    let prettyTimestamp: String
+    let prettyTimestamp: String?
     let device: String
 
     static func stamp() -> Sidecar {
